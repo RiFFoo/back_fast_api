ae84db3575ac44912c81456316ee76a46a6b8d4b
Рифат Фарухшин <rifatfarukhshin@MacBook-Pro-Rifat.local>
2024-10-22 22:51:26 +0300

    pagination

--- hotels.py
+++ hotels.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 from fastapi import Query, APIRouter
 from schemas.hotels import Hotel, HotelPatch
 
@@ -6,21 +8,44 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 hotels = [
     {"id": 1, "title": "Sochi", "name": "Polyana"},
     {"id": 2, "title": "Дубай", "name": "Burj"},
+
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
 ]
 
 @router.get("")
 def get_hotels(
         id: int | None = Query(None, description="Айдишник"),
         title: str | None = Query(None, description="Название"),
+        page: int | None = Query(None, description="Страница"),
+        per_page: int | None = Query(None, description="Количество на странице"),
 ):
-    hotels_ = []
-    for hotel in hotels:
-        if id and hotel["id"] != id:
-            continue
-        if title and hotel["title"] != title:
-            continue
-        hotels_.append(hotel)
-    return hotels_
+    count_per_paige = per_page
+    count_paige = page
+    id = id
+    title = title
+
+    if count_per_paige > 0:
+        a = ((count_paige - 1) * count_per_paige)
+        if count_paige > 0:
+            return hotels[a:(a + count_per_paige)]
+    else:
+        return hotels[0:3] # стандарт при не заполненной per_page
+
+    if id > 0: # надо переделать, чтобы работал общий список тоже
+        hotels_ = []
+        for hotel in hotels:
+            if id and hotel["id"] != id:
+                continue
+            if title and hotel["title"] != title:
+                continue
+            hotels_.append(hotel)
+        return hotels_
 
 @router.delete("{hotel_id}")
 def delete_hotel(hotel_id: int):
