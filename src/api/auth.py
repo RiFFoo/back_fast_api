0f543c698b7242f7d2c457f715195c632223a5fb
Рифат Фарухшин <rifatfarukhshin@MacBook-Air.local>
2026-03-15 17:45:02 +0500

    add save token in cookie & create decode token method

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response, Request
 
 from src.database import async_session_maker
+
+from src.api.dependencies import UserIdDep
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
 from src.services.auth import AuthService
@@ -21,6 +23,7 @@ async def register_user(
 @router.post("/login")
 async def login_user(
         data: UserRequestAdd,
+        response: Response,
 ):
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
@@ -29,4 +32,14 @@ async def login_user(
         if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль неверный")
         access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+
+@router.get("/me")
+async def get_me(
+        user_id: UserIdDep,
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(id=user_id)
+        return user
+
