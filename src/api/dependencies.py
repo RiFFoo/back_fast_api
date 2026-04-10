59e44f60f061a1a58985298a0e8aa2255de84f36
Рифат Фарухшин <rifatfarukhshin@MacBook-Air.local>
2026-04-10 20:35:03 +0500

    create context manager & utils/db_manager.py

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -3,7 +3,10 @@ from typing import Annotated
 from fastapi import Depends, Query, Request, HTTPException
 from pydantic import BaseModel
 
+from src.database import async_session_maker
 from src.services.auth import AuthService
+from src.utils.db_manager import DBManager
+
 
 class PaginationParams(BaseModel):
     page: Annotated[int | None, Query( 1, ge=1)]
@@ -26,11 +29,11 @@ UserIdDep = Annotated[int, Depends(get_current_user_id)]
 
 
 def get_db_manager():
-    return DBManager()
+    return
 
 
 async def get_db():
-    async with get_db_manager() as db:
+    async with DBManager(session_factory=async_session_maker) as db:
         yield db
 
 
--- src/api/hotels.py
+++ src/api/hotels.py
@@ -4,7 +4,7 @@ from src.database import async_session_maker
 from src.repositories.hotels import HotelsRepository
 from src.schemas.hotels import HotelPatch, HotelAdd
 
-from src.api.dependencies import PaginationDep
+from src.api.dependencies import PaginationDep, DBDep
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
